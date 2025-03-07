53a069c5ef41d869d03f49154214eca6a0a83022
michael15346 <mzadorskyy@gmail.com>
2025-03-07 09:12:38 +0300

    wip: corner tracing

--- wavelets.py
+++ wavelets.py
@@ -131,21 +131,65 @@ def idwt(a: OffsetMatrix, d: tuple[OffsetMatrix, ...], w: Wavelet):
     return ai
 
 
+def downsample_corners(a: OffsetMatrix, coords: tuple[float, float, float, float], w: Wavelet):
+    Minv = np.linalg.inv(w.M)
+    x1 = Minv @ np.array([a.offset[0], a.offset[1]])
+    x2 = Minv @ np.array([a.offset[0] + a.matrix.shape[1]-1, a.offset[1]])
+    x3 = Minv @ np.array([a.offset[0], a.offset[1] - a.matrix.shape[0]+1])
+    x4 = Minv @ np.array([a.offset[0] + a.matrix.shape[1]-1, a.offset[1] - a.matrix.shape[0]+1])
+    xmin = ceil(min(x1[0], x2[0], x3[0], x4[0]))
+    ymax = floor(max(x1[1], x2[1], x4[1], x4[1]))
+    ((x1, y1), (x2, y2), (x3, y3), (x4, y4)) = coords
+    scaled_dl = (Minv @ np.array([x1, y1]))
+    scaled_dr = (Minv @ np.array([x2, y2]))
+    scaled_ul = (Minv @ np.array([x3, y3]))
+    scaled_ur = (Minv @ np.array([x4, y4]))
+    dl = tuple(to_python(scaled_dl[0], scaled_dl[1], np.array([xmin, ymax])))
+    dr = tuple(to_python(scaled_dr[0], scaled_dr[1], np.array([xmin, ymax])))
+    ul = tuple(to_python(scaled_ul[0], scaled_ul[1], np.array([xmin, ymax])))
+    ur = tuple(to_python(scaled_ur[0], scaled_ur[1], np.array([xmin, ymax])))
+    return (dl, dr, ul, ur)
+
+
+def upsample_corners(a: OffsetMatrix, coords: tuple[float, float, float, float], w: Wavelet):
+    x1 = M @ np.array([a.offset[0], a.offset[1]])
+    x2 = M @ np.array([a.offset[0] + a.matrix.shape[0] - 1, a.offset[1]])
+    x3 = M @ np.array([a.offset[0], a.offset[1] - a.matrix.shape[0]+1])
+    x4 = M @ np.array([a.offset[0] + a.matrix.shape[1]  - 1, a.offset[1] - a.matrix.shape[0] + 1])
+    xmin = int(min(x1[0], x2[0], x3[0], x4[0]))
+    ymax = int(max(x1[1], x2[1], x3[1], x4[1]))
+    ((x1, y1), (x2, y2), (x3, y3), (x4, y4)) = coords
+    scaled_dl = (M @ np.array([x1, y1]))
+    scaled_dr = (M @ np.array([x2, y2]))
+    scaled_ul = (M @ np.array([x3, y3]))
+    scaled_ur = (M @ np.array([x4, y4]))
+    dl = tuple(to_python(scaled_dl[0], scaled_dl[1], np.array([xmin, ymax])))
+    dr = tuple(to_python(scaled_dr[0], scaled_dr[1], np.array([xmin, ymax])))
+    ul = tuple(to_python(scaled_ul[0], scaled_ul[1], np.array([xmin, ymax])))
+    ur = tuple(to_python(scaled_ur[0], scaled_ur[1], np.array([xmin, ymax])))
+    return (dl, dr, ul, ur)
+
+
 def wavedec(a0: OffsetMatrix, n: int, w: Wavelet):
     a = a0
     d = list()
+    x0 = a0.offset[0]
+    y0 = a0.offset[0]
+    (m, n) = a0.matrix.shape
+    corners = ((x0, y0), (x0, y0 + n), (x0 + m, y0), (x0 + m, y0 + n))
     for i in range(n):
+        corners = downsample_corners(a, corners, w)
         (a, di) = dwt(a, w)
         d.append(di)
-    return (a, d)
+    return (a, d, corners)
 
 
-def waverec(a: OffsetMatrix, d: list[OffsetMatrix], w: Wavelet):
+def waverec(a: OffsetMatrix, d: list[OffsetMatrix], w: Wavelet, corners: tuple[tuple[float, float], ...]):
     ai = a
     for di in reversed(d):
+        corners = upsample_corners(ai, corners, w)
         ai = idwt(ai, di, w)
-
-    return ai
+    return (ai, corners)
 
 #data = OffsetMatrix(iio.imread('http://upload.wikimedia.org/wikipedia/commons/d/de/Wikipedia_Logo_1.0.png'), np.array([0,0]))
 data = OffsetMatrix(255 * np.array([[1, 1], [1, 1]]), np.array([0,0]))
@@ -162,8 +206,10 @@ hdual_conj = OffsetMatrix(np.array([[-0.125], [0.25], [0.75], [0.25], [-0.125]])
 gdual_conj = (OffsetMatrix(np.array([[-0.25], [0.5], [-0.25]]),np.array([0,0])),)
 w = Wavelet(h, g, hdual_conj, gdual_conj, M, np.abs(np.linalg.det(M)))
 
-(ai, d) = wavedec(data, 3, w)
+(ai, d, corners_dec) = wavedec(data, 5, w)
 
-a = waverec(ai, d, w)
+(a, corners) = waverec(ai, d, w, corners_dec)
 print(a.matrix)
+print(corners)
+print(to_python(corners[1][0], corners[1][1], a.offset))
 
