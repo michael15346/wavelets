fix: bayes/visu sigma computation

batched.py
@@ -133,11 +133,11 @@ def wavedec_period_batched(data: OffsetTensor, w: Wavelet, level: int):
     wave = []
     for l in range(div):
         res = wavedec_period_batch(data, w, order)
-        wave.extend(res[1:])
+        wave[:0] = res[1:]
         data = res[0]
     if rem > 0:
         res = wavedec_period_fastest(data, w, rem)
-        wave.extend(res[1:])
+        wave[:0] = res[1:]
     wave.insert(0, res[0])
     return wave
 
@@ -161,11 +161,11 @@ def waverec_period_batched(c: list, w: Wavelet, original_shape):
         shape //= np.abs(Mpd)
     img = c[0]
     if rem > 0:
-        coef = [img] + c[-rem:]
+        coef = [img] + c[1:1+rem]
         img = waverec_period_fastest(coef, w, shape)
 
-    for i, idx in enumerate(range(len(c) - rem, 1, -order)):
-        cc = c[idx-order:idx]
+    for i, idx in enumerate(range(1+rem, len(c), order)):
+        cc = c[idx:idx+order]
         coef = [img] + cc
         img = waverec_period_batch(coef, w, shapes[-i-1])
     slices = tuple(slice(-o, -o + s) for s, o in zip(original_shape, img.offset))

denoise.py
@@ -29,7 +29,7 @@ def bayes_thresh(detail, var):
 
 def apply_bayes_thresh(wavecoef):
     details = wavecoef[1:]
-    details_flat = np.concatenate(list(itertools.chain(*itertools.chain(*wavecoef[1:]))), axis=None)
+    details_flat = np.concatenate(list(itertools.chain(*itertools.chain(*wavecoef[-1:]))), axis=None)
     sigma = est_sigma(details_flat)
     var = sigma * sigma
     denoised = [wavecoef[0]] + [[bayes_thresh(c, var) for c in level_coef] for level_coef in details]
@@ -49,7 +49,7 @@ def universal_thresh(img, wavecoef):
     return sigma * np.sqrt(2 * np.log(img.tensor.size))
 
 def universal_thresh_1d(img, wavecoef):
-    details = wavecoef[1:]
+    details = wavecoef[-1:]
     sigma = est_sigma_1d(details)
     return sigma * np.sqrt(2 * np.log(img.size))
 

wavelets.py
@@ -103,7 +103,7 @@ def benchmark_denoise(content):
         data_gaussian = OffsetTensor(data_gaussian, np.array([0,0]))
         data_snp = OffsetTensor(data_snp, np.array([0,0]))
 
-        for level in (5,):  # range(1,6):
+        for level in (13,):  # range(1,6):
             ci = wavedec_period_batched(data, w, level)
             ci_gaussian = wavedec_period_batched(data_gaussian, w, level)
             ci_snp = wavedec_period_batched(data_snp, w, level)
@@ -332,7 +332,7 @@ if __name__ == "__main__":
     elif args.command == 'benchmark_denoise':
         with open("WaveDB.json", 'r') as j:
             contents = json.loads(j.read())
-        #results_nonflat = list(map(benchmark_denoise, contents[101:]))
+        results_nonflat = list(map(benchmark_denoise, contents))
         discrete_wavelets = pywt.wavelist(kind='discrete')
         results_1d = list(map(benchmark1D_denoise, discrete_wavelets))
         #results = list(chain(*results_nonflat)) #+ list(chain(*results_1d))